aff863cf9848b12b59c3849625f582721d6fafcf
Isaac Parks <isaac@iphone.sc.loc>
2021-11-27 13:34:38 -0600

    routes

--- src/app.jsx
+++ src/app.jsx
@@ -1,8 +1,10 @@
+import Treatments from "./treatments/treatments.jsx";
+
 import Header from "./header/header";
 import Home from "./homepage/home";
 import Footer from "./footer/footer";
 import { useState } from "react";
-
+import { Route, Routes } from "react-router-dom";
 export default function App() {
   const [showMenu, setShowMenu] = useState(false);
 
@@ -16,7 +18,11 @@ export default function App() {
         changeMenu={changeMenu}
         showMenu={showMenu ? "menu show" : "menu"}
       />
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/treatments" element={<Treatments />} />
+      </Routes>
+
       <Footer />
     </>
   );
