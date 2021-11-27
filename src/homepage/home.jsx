6b681772a6e1c66e3f1ae355cc7058321c78bfdc
Isaac Parks <isaac@iphone.sc.loc>
2021-11-27 11:37:06 -0600

    home section changes

--- src/homepage/home.jsx
+++ src/homepage/home.jsx
@@ -11,7 +11,7 @@ export default function Home() {
   useEffect(() => {
     const setInt = setInterval(() => {
       setArrow(!arrow);
-    }, 2000);
+    }, 1000);
 
     return () => {
       clearInterval(setInt);
