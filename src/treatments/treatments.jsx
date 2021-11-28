00e71d6e26f8b12528afb752bc41e1630b3cd2d2
Isaac Parks <isaacparks1800@gmail.com>
2021-11-28 12:22:58 -0600

    fixed issue with treatment header

--- src/treatments/treatments.jsx
+++ src/treatments/treatments.jsx
@@ -1,10 +1,20 @@
-import styles from "./treatments.css";
 import Summary from "../homepage/summary";
 import chiropractic from "../images/chiropractic-2.png";
 export default function Treatments() {
   return (
     <div className="treatments">
-      <h1 className="treatments-title">Treatments</h1>
+      <h1
+        style={{
+          borderBottom: "2px solid rgb(174, 220, 247)",
+          textAlign: "center",
+          padding: "10px",
+          paddingTop: "1.5rem",
+          paddingBottom: "1.5rem",
+          fontSize: "2.5rem",
+        }}
+      >
+        Treatments
+      </h1>
       <Summary
         title="General Chiropractic"
         content="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
